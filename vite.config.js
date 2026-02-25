e11f6ba1dcae10b2d8363a2297b7d100a4efc187
Joao <joao.dequeiro@gmail.com>
2026-02-25 01:20:28 -0300

    Add Vite e configuração da Vercel

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        educacao: resolve(__dirname, 'educacao.html'),
+        setorPublico: resolve(__dirname, 'setor-publico.html'),
+        setorPrivado: resolve(__dirname, 'setor-privado.html'),
+        adocao: resolve(__dirname, 'adocao-cientifica.html'),
+        parceiros: resolve(__dirname, 'parceiros.html'),
+        termos: resolve(__dirname, 'termos-de-licenciamento.html')
+      }
+    }
+  }
+});
